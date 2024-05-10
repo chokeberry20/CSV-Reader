5b12525d2a87aac7d1661d336839e9e1ac060aaf
Andrew <andriu.gabor228@gmail.com>
2024-05-10 16:54:24 +0300

    Add DB and program logic

---
+++ Models/Cab.cs
@@ -0,0 +1,29 @@
+﻿using CsvHelper.Configuration.Attributes;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace CSV_Reader.Models
+{
+    public class Cab
+    {
+        [Name("tpep_pickup_datetime")]
+        public DateTime PickUpTime { get; set; }
+        [Name("tpep_dropoff_datetime")]
+        public DateTime DropOffTime { get; set; }
+        [Name("passenger_count")]
+        public string? PassengerCount { get; set; }
+        [Name("trip_distance")]
+        public double TripDistance { get; set; }
+        [Name("store_and_fwd_flag")]
+        public string? StoreAndFwdFlag { get; set; }
+        [Name("PULocationID")]
+        public ushort PickUpLocationId { get; set; }
+        [Name("DOLocationID")]
+        public ushort DropOffLocationId { get; set; }
+        [Name("fare_amount")]
+        public decimal FareAmount { get; set; }
+        [Name("tip_amount")]
+        public decimal TipAmount { get; set; }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using CSV_Reader.Models;
+using CsvHelper;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace CSV_Reader
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            const string absolutePath = "C:\\Users\\andri\\Downloads\\sample-cab-data.csv";           
+
+            var list = CsvRead(absolutePath);
+
+            
+        }
+
+        static public IEnumerable<Cab> CsvRead(string path)
+        {
+            var list = new List<Cab>();
+
+            using (var reader = new StreamReader(path))
+            using (var csvReader = new CsvReader(reader, CultureInfo.InvariantCulture))
+            {
+                var records = csvReader.GetRecords<Cab>();
+
+                using (ApplicationContext db = new ApplicationContext())
+                {
+                    foreach (var item in records)
+                    {
+                        list.Add(item);
+                    }
+                    foreach (var item in list)
+                    {
+                        db.Cabs.Add(item);
+                    }
+                    db.SaveChanges();
+                }
+
+                
+            }
+
+            return list;
+        }
+    }
+}
